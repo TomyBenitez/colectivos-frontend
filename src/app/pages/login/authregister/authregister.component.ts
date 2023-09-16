ba1350eb48c87b10dc70fe1ca744df36bceedace
TomyBenitez <tomybenitez7@gmail.com>
2023-09-16 13:48:38 -0300

    showError in html of register page

--- src/app/pages/login/authregister/authregister.component.ts
+++ src/app/pages/login/authregister/authregister.component.ts
@@ -16,12 +16,24 @@ export class AuthregisterComponent {
     repetircontrasena:''
   };
 
+  messageError:string='';
+  showError:boolean=false;
 
 
   submitForm(){
-    if(this.formData.correo !== this.formData.repetircorreo) return; //si los correos no coinciden retorna
-    if(this.formData.contrasena !== this.formData.repetircontrasena) return; //si las contraseñas no coinciden retorna
+    if(this.formData.correo !== this.formData.repetircorreo){
+      this.messageError = 'Los correos no coinciden';
+      this.showError = true;
+      return;
+    } //si los correos no coinciden retorna
+
+    if(this.formData.contrasena !== this.formData.repetircontrasena){
+      this.messageError = 'Las contraseñas no coinciden';
+      this.showError = true;
+      return;
+    } //si las contraseñas no coinciden retorna
     
+    this.showError = false;
     console.log(this.formData)
   }
 }
