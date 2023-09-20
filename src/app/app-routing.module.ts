0e3a46f64066483f889c01845903ec79c96f1701
TomyBenitez <tomybenitez7@gmail.com>
2023-09-20 17:09:32 -0300

    estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 import { AuthloginComponent } from './pages/login/authlogin/authlogin.component';
 import { AuthregisterComponent } from './pages/login/authregister/authregister.component';
-import { AuthGuard } from './guard/auth.guard';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full' , redirectTo: 'home'},
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: 'search', loadChildren: () => import ('./pages/search/search.module').then(m => m.SearchModule)},
   {path: 'admin', loadChildren:()=> import('./pages/admin/admin.module').then(m=>m.AdminModule), canActivate:[AuthGuard]},
   {path: 'authlogin', component:AuthloginComponent},
-  {path: 'authregister', component: AuthregisterComponent},
+  {path: 'authregister', component:AuthregisterComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
